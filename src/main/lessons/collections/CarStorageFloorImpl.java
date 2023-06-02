bdcebbecfbfd6524db877bf8fa9b538bd31331cb
timur <timur.petrov.work@gmail.com>
2023-06-02 19:56:08 +0300

    что смог то и сделал

--- src/main/lessons/collections/CarStorageFloorImpl.java
+++ src/main/lessons/collections/CarStorageFloorImpl.java
@@ -5,6 +5,19 @@ import lessons.car.Car;
 import java.util.HashMap;
 
 public class CarStorageFloorImpl extends HashMap<String, Car> implements CarStorageFloor {
+    public int num;
+    public int size;
+
+    public CarStorageFloorImpl(int size, int num) {
+        super(size);
+        int firstNum=(size * num);
+        int lastNum=(size * (num + 1);
+        for (int i = firstNum; i < lastNum; i++) {
+
+            put(String.valueOf(i), null);
+        }
+
+    }
 
     @Override
     public int getParkingLotsSize() {
@@ -14,40 +27,33 @@ public class CarStorageFloorImpl extends HashMap<String, Car> implements CarStor
     @Override
     public String getAvailableParkingLot() {
         for (Entry<String, Car> stringCarEntry : this.entrySet()) {
-            if (stringCarEntry.getValue() == null){
+            if (stringCarEntry.getValue() == null) {
 
                 return stringCarEntry.getKey();
             }
         }
         return null;
     }
-    boolean park(Car car, String place){
 
-            if (this.containsKey(place)){
-                if (get(place)== null){
-                  put(place,car);
-                  return true;
-                }
+    boolean park(Car car, String place) {
 
+        if (this.containsKey(place)) {
+            if (get(place) == null) {
+                put(place, car);
+                return true;
             }
+
+        }
 
         return false;
     }
-    public Car getCar(String place){
-        var a =get(place);
-                put(place,null);
-                return a;
-    }
-   public int num;
-    public int size;
-    public CarStorageFloorImpl(int size,int num) {
-        super(size);
-        for (int i = (size*num); i <(size*(num+1)); i++) {
 
-            put(String.valueOf(i),null);
-        }
-
+    public Car getCar(String place) {
+        var a = get(place);
+        put(place, null);
+        return a;
     }
+
 
 }
 
--- src/main/lessons/collections/CarStorageImpl.java
+++ src/main/lessons/collections/CarStorageImpl.java
@@ -8,47 +8,8 @@ import java.util.LinkedList;
 
 public class CarStorageImpl extends LinkedList<CarStorageFloorImpl> implements CarStorage {
 
-    @Override
-    public int getHeight() {
-        return size();
-    }
-
-    ;
-
-    @Override
-    public int getAmountPerFloor() {
-        return amountPerFloor;
-    }
-
-    ;
-
-    @Override
-    public Car getCarFromStorage(String placeNumber) {
-        for (int i = 0; i < size(); i++){
-            var in= Integer.parseInt(placeNumber);
-            var a = (in-(in % getHeight()))/getHeight();
-            return get(a).getCar(placeNumber);
-        }
-      return null;
-    }
-
-    ;
-
-    @Override
-    public boolean parkCarToStorage(Car car, String parkingLotNumber) {
-        for (int i = 0; i < size(); i++) {
-            if (get(i).park(car, parkingLotNumber))
-
-                return true;
-        }
-        return false;
-    }
-
-
-
     int amountPerFloor;
     int height;
-
     public CarStorageImpl(int amountPerFloor, int height) {
         this.amountPerFloor = amountPerFloor;
         this.height = height-1;
@@ -56,4 +17,36 @@ public class CarStorageImpl extends LinkedList<CarStorageFloorImpl> implements C
             add(i, new CarStorageFloorImpl(amountPerFloor, i));
         }
     }
+    @Override
+    public int getHeight() {
+        return size();
+    }
+
+
+
+    @Override
+    public int getAmountPerFloor() {
+        return amountPerFloor;
+
+    }
+
+
+
+    @Override
+    public Car getCarFromStorage(String placeNumber) {
+            var floorNum= Integer.parseInt(placeNumber);
+            var floor = (floorNum-(floorNum % getHeight()))/getHeight();
+            return get(floor).getCar(placeNumber);
+    }
+
+
+
+    @Override
+    public boolean parkCarToStorage(Car car, String parkingLotNumber) {
+        var floorNum= Integer.parseInt(parkingLotNumber);
+        var floor = get((floorNum-(floorNum % getHeight()))/getHeight());
+        return floor.park(car,parkingLotNumber);
+    }
+
+
 }
